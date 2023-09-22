ea001a8c6f9488d36856f7a648b68cd7597b405c
ETS <ct67ca@bosch.com>
2023-09-22 10:17:11 -0300

    Configuração externalizada

--- src/main/java/com/secao01/secao01/Secao01Application.java
+++ src/main/java/com/secao01/secao01/Secao01Application.java
@@ -2,6 +2,7 @@ package com.secao01.secao01;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -14,8 +15,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @RestController
 public class Secao01Application {
 
-	@Autowired
-	@Qualifier("applicationName") // Pega essa configuração do sistema
+	// @Autowired
+	// @Qualifier("applicationName") // Pega essa configuração do sistema
+	@Value("${application.name}")
 	private String applicationName;
 
 	@GetMapping("/hello")
