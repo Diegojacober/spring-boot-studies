564d268a7e71fc749d47c052dec33e8d85ea6dad
Diego <diegoalencar.jacober@gmail.ocm>
2023-10-04 20:35:20 -0300

    Mapeando pedido

--- springcomjpa/src/main/java/com/diegojacober/jpa/demo/domain/entity/Cliente.java
+++ springcomjpa/src/main/java/com/diegojacober/jpa/demo/domain/entity/Cliente.java
@@ -1,14 +1,17 @@
 package com.diegojacober.jpa.demo.domain.entity;
 
+import java.util.Set;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "cliente") //não é obrigatório desde que o nome da tabela não seja diferente
+@Table(name = "cliente") // não é obrigatório desde que o nome da tabela não seja diferente
 public class Cliente {
 
     @Id
@@ -18,6 +21,9 @@ public class Cliente {
 
     @Column(name = "nome", length = 100, nullable = false)
     private String nome;
+
+    @OneToMany(mappedBy = "cliente")
+    private Set<Pedido> pedidos;
 
     public Cliente() {
     }
@@ -43,8 +49,17 @@ public class Cliente {
         this.nome = name;
     }
 
+    public Set<Pedido> getPedidos() {
+        return pedidos;
+    }
+
+    public void setPedidos(Set<Pedido> pedidos) {
+        this.pedidos = pedidos;
+    }
+
     @Override
     public String toString() {
         return "Cliente [id=" + id + ", nome=" + nome + "]";
     }
+
 }
--- springcomjpa/src/main/java/com/diegojacober/jpa/demo/domain/entity/Pedido.java
+++ springcomjpa/src/main/java/com/diegojacober/jpa/demo/domain/entity/Pedido.java
@@ -3,10 +3,30 @@ package com.diegojacober.jpa.demo.domain.entity;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "pedido")
 public class Pedido {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "cliente_id")
     private Cliente cliente;
+
+    @Column(name = "data_pedido")
     private LocalDate dataPedido;
+    
+    @Column(name = "total", length = 20, precision = 2)
     private BigDecimal total;
 
     public Integer getId() {
