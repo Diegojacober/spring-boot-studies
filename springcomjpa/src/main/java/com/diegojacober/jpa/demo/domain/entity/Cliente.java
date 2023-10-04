c682d414cd6cccf78f14a99e6b1f56f835d06366
ETS <ct67ca@bosch.com>
2023-10-04 11:15:29 -0300

    Mapeando dados para o JPA

--- springcomjpa/src/main/java/com/diegojacober/jpa/demo/domain/entity/Cliente.java
+++ springcomjpa/src/main/java/com/diegojacober/jpa/demo/domain/entity/Cliente.java
@@ -1,7 +1,22 @@
 package com.diegojacober.jpa.demo.domain.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "cliente") //não é obrigatório desde que o nome da tabela não seja diferente
 public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
     private Integer id;
+
+    @Column(name = "nome", length = 100, nullable = false)
     private String nome;
 
     public Cliente() {
