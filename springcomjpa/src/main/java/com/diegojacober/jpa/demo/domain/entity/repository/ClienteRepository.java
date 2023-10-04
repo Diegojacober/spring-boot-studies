2c64e89d56e6db22c7955aa89503fc6253d29b9b
Diego <diegoalencar.jacober@gmail.ocm>
2023-10-04 19:58:13 -0300

    Mapeando para JPA

--- springcomjpa/src/main/java/com/diegojacober/jpa/demo/domain/entity/repository/ClienteRepository.java
+++ springcomjpa/src/main/java/com/diegojacober/jpa/demo/domain/entity/repository/ClienteRepository.java
@@ -8,8 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.diegojacober.jpa.demo.domain.entity.Cliente;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 
 @Repository
 public class ClienteRepository {
@@ -23,39 +27,62 @@ public class ClienteRepository {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
+    @Autowired
+    private EntityManager entityManager;
+
+    @Transactional
     public Cliente salvar(Cliente cliente) {
-        jdbcTemplate.update(INSERT, new Object[] { cliente.getNome() });
+        // jdbcTemplate.update(INSERT, new Object[] { cliente.getNome() });
+        entityManager.persist(cliente);
         return cliente;
     }
 
+    @Transactional
     public Cliente atualizar(Cliente cliente) {
-        jdbcTemplate.update(UPDATE, new Object[] { cliente.getNome(), cliente.getId() });
+        // jdbcTemplate.update(UPDATE, new Object[] { cliente.getNome(), cliente.getId()
+        // });
+        entityManager.merge(cliente);
         return cliente;
     }
 
+    @Transactional
     public void deletar(Cliente cliente) {
-        deletar(cliente.getId());
+        // deletar(cliente.getId());
+        if (!entityManager.contains(cliente)) {
+            cliente = entityManager.merge(cliente);
+        }
+        entityManager.remove(cliente);
     }
 
+    @Transactional
     public void deletar(Integer id) {
-        jdbcTemplate.update(DELETE, id);
+        // jdbcTemplate.update(DELETE, id);
+        Cliente cliente = entityManager.find(Cliente.class, id);
+        deletar(cliente);
     }
 
+    @Transactional(readOnly = true)
     public List<Cliente> buscarPorNome(String nome) {
-        return jdbcTemplate.query(SELECT_POR_NOME, new Object[]{"%" + nome + "%"}, obterClienteMapper());
+        // return jdbcTemplate.query(SELECT_POR_NOME, new Object[]{"%" + nome + "%"},
+        // obterClienteMapper());
+        String jpql = " select c from Cliente c where c.nome like :nome ";
+        TypedQuery<Cliente> query = entityManager.createQuery(jpql, Cliente.class);
+        query.setParameter("nome", "%" + nome + "%");
+        return query.getResultList();
     }
 
+    @Transactional(readOnly = true)
     public List<Cliente> obterTodos() {
-        return jdbcTemplate.query(SELECT_ALL, obterClienteMapper());
-
+        // return jdbcTemplate.query(SELECT_ALL, obterClienteMapper());
+        return entityManager.createQuery("from Cliente", Cliente.class).getResultList();
     }
 
-    private RowMapper<Cliente> obterClienteMapper() {
-        return new RowMapper<Cliente>() {
-            @Override
-            public Cliente mapRow(ResultSet resultSet, int i) throws SQLException {
-                return new Cliente(resultSet.getInt("id"), resultSet.getString("nome"));
-            }
-        };
-    }
+    // private RowMapper<Cliente> obterClienteMapper() {
+    // return new RowMapper<Cliente>() {
+    // @Override
+    // public Cliente mapRow(ResultSet resultSet, int i) throws SQLException {
+    // return new Cliente(resultSet.getInt("id"), resultSet.getString("nome"));
+    // }
+    // };
+    // }
 }
