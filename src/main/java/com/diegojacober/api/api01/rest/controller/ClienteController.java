ae5ae35b0f6aa9a59ae8b32228ec3400d3574013
Diego <diegoalencar.jacober@gmail.ocm>
2023-10-07 12:01:18 -0300

    Criando o controller

---
+++ src/main/java/com/diegojacober/api/api01/rest/controller/ClienteController.java
@@ -0,0 +1,20 @@
+package com.diegojacober.api.api01.rest.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller // forma01
+@RequestMapping("/api/clientes") //forma01
+public class ClienteController {
+
+    @RequestMapping(value = "/hello/{nome}", method = RequestMethod.GET)
+    @ResponseBody
+    public String helloCliente(@PathVariable("nome") String nomeCliente) {
+        return String.format("Hello %s ", nomeCliente);
+    }
+}
