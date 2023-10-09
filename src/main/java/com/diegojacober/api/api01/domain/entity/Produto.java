8ff4d48fe012ad831ac25e6e2305cbdfcf7d8b8b
Diegojacober <diegoalencar.jacober@gmail.com>
2023-10-09 13:54:58 -0300

    Utilizando projeto lombok

--- src/main/java/com/diegojacober/api/api01/domain/entity/Produto.java
+++ src/main/java/com/diegojacober/api/api01/domain/entity/Produto.java
@@ -8,9 +8,23 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Table(name = "produto")
+// @Getter
+// @Setter
+// @EqualsAndHashCode
+// @ToString
+@Data // substitui algumas outras annotations
+@NoArgsConstructor
+@AllArgsConstructor
 public class Produto {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -22,37 +36,13 @@ public class Produto {
     @Column(name = "preco_unitario")
     private BigDecimal preco;
 
-    public Produto(Integer id, String descricao, BigDecimal preco) {
-        this.id = id;
-        this.descricao = descricao;
-        this.preco = preco;
-    }
+    // public Produto(Integer id, String descricao, BigDecimal preco) {
+    //     this.id = id;
+    //     this.descricao = descricao;
+    //     this.preco = preco;
+    // }
 
-    public Produto() {
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
-    public BigDecimal getPreco() {
-        return preco;
-    }
-
-    public void setPreco(BigDecimal preco) {
-        this.preco = preco;
-    }
+    // public Produto() {
+    // }
 
 }
--- src/main/java/com/diegojacober/api/api01/rest/controller/dto/PedidoDTO.java
+++ src/main/java/com/diegojacober/api/api01/rest/controller/dto/PedidoDTO.java
@@ -3,6 +3,13 @@ package com.diegojacober.api.api01.rest.controller.dto;
 import java.math.BigDecimal;
 import java.util.List;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class PedidoDTO {
     private Integer cliente;
     private BigDecimal total;
