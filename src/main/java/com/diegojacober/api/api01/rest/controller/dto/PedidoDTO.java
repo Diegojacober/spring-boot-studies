9673772b5153e76fc135eaccd6d41d2aaabed444
Diegojacober <diegoalencar.jacober@gmail.com>
2023-10-09 16:47:10 -0300

    Criando uma validation personalizada

--- src/main/java/com/diegojacober/api/api01/rest/controller/dto/PedidoDTO.java
+++ src/main/java/com/diegojacober/api/api01/rest/controller/dto/PedidoDTO.java
@@ -3,6 +3,8 @@ package com.diegojacober.api.api01.rest.controller.dto;
 import java.math.BigDecimal;
 import java.util.List;
 
+import com.diegojacober.api.api01.validation.NotEmptyList;
+
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -14,7 +16,10 @@ import lombok.NoArgsConstructor;
 public class PedidoDTO {
     @NotNull(message = "Informe o código do cliente.")
     private Integer cliente;
+
     @NotNull(message = "Campo Total do pedido é obrigatório.")
     private BigDecimal total;
+
+    @NotEmptyList(message = "Pedido não pode ser realizado sem itens.")
     private List<ItemPedidoDTO> items;
 }
