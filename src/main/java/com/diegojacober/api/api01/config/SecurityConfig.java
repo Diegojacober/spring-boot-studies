d793b1912e0b56c254b13ec19e8718128d4ae5db
Diego <diegoalencar.jacober@gmail.ocm>
2023-10-10 20:52:43 -0300

    Adicionando configurações

---
+++ src/main/java/com/diegojacober/api/api01/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.diegojacober.api.api01.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+// https://spring.io/blog/2022/02/21/spring-security-without-the-websecurityconfigureradapter
+public class SecurityConfig{
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests((authz) -> authz
+                .anyRequest().authenticated()
+            )
+            .httpBasic(withDefaults());
+        return http.build();
+    }
+}
