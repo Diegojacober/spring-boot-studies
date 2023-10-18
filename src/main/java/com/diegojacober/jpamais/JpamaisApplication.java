24edb754a06c0857a17624c878b7c181b9e4a552
Diegojacober <diegoalencar.jacober@gmail.com>
2023-10-18 16:06:04 -0300

    Aprofundando em JPA

--- src/main/java/com/diegojacober/jpamais/JpamaisApplication.java
+++ src/main/java/com/diegojacober/jpamais/JpamaisApplication.java
@@ -5,30 +5,24 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.diegojacober.jpamais.domain.repository.CidadeRepository;
+import com.diegojacober.jpamais.domain.entity.Cidade;
+import com.diegojacober.jpamais.service.CidadeService;
 
 
 @SpringBootApplication
 public class JpamaisApplication implements CommandLineRunner{
 
 	@Autowired
-	private CidadeRepository cidadeRepository;
+	private CidadeService cidadeService;
 
 	@Override
 	public void run(String... args) throws Exception {
-		listarCidades();
-	}
-
-	void listarCidades() {
-		cidadeRepository.findAll().forEach(System.out::println);
-	}
-
-	void listarCidadesPorHabitantes() {
-		cidadeRepository.findByHabitantes(999999L);
-	}
-
-	void listarCidadesPorNome() {
-		cidadeRepository.findByNome("Campinas");
+		cidadeService.listarCidades();
+		// cidadeService.listarCidadesByNomeSpecs();
+		cidadeService.listarCidadeSpecsFiltroDinamico(new Cidade(null, "São Paulo", 123L));
+		// cidadeService.listarCidadesPorNome();
+		// cidadeService.listarCidadesPorHabitantes();
+		// cidadeService.filtroDinamico(new Cidade(null, "Cor", 48L)).forEach(System.out::println);;
 	}
 
 	public static void main(String[] args) {
--- src/main/java/com/diegojacober/jpamais/domain/repository/CidadeRepository.java
+++ src/main/java/com/diegojacober/jpamais/domain/repository/CidadeRepository.java
@@ -2,14 +2,19 @@ package com.diegojacober.jpamais.domain.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.diegojacober.jpamais.domain.entity.Cidade;
 
-public interface CidadeRepository extends JpaRepository<Cidade, Long>{
+public interface CidadeRepository extends JpaRepository<Cidade, Long>, JpaSpecificationExecutor<Cidade>{
     //Buscar pelo nome correto
-    List<Cidade> findByNome(String nome);
+    List<Cidade> findByNome(String nome, Sort sort);
 
     List<Cidade> findByHabitantes(Long habitantes);
     
@@ -20,10 +25,28 @@ public interface CidadeRepository extends JpaRepository<Cidade, Long>{
     List<Cidade> findByNomeEndingWith(String nome);
 
     // Contendo 
-    List<Cidade> findByNomeContaining(String nome);
+    List<Cidade> findByNomeContaining(String nome, Sort sort);
 
     // Busca pelo like 
     // tem que passar as % de como quer igual no sql
-    @Query("select c from Cidade c where lower(c.nome) like lower(?l)") // para poder trabalhar com case
-    List<Cidade> findByNomeLike(String l);
+    @Query("select c from Cidade c where lower( c.nome) like lower(:l)") // para poder trabalhar com case
+    List<Cidade> findByNomeLike(@Param("l") String l);
+
+    // Menor que
+    List<Cidade> findByHabitantesLessThan(Long habitantes);
+
+    // Mais que
+    List<Cidade> findByHabitantesGreaterThan(Long habitantes);
+
+     // Menor ou igual a
+    List<Cidade> findByHabitantesLessThanEqual(Long habitantes);
+
+    // Maior ou igual a
+    Page<Cidade> findByHabitantesGreaterThanEqual(Long habitantes, Pageable sort);
+
+
+    // Maior ou igual a e contem no nome
+    List<Cidade> findByHabitantesGreaterThanEqualAndNomeContaining(Long habitantes, String nome);
+    
+    
 }
