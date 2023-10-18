ae64a2b0014cf5b97e544551f507fdbd79859352
Diegojacober <diegoalencar.jacober@gmail.com>
2023-10-18 13:45:13 -0300

    Criando branch

--- src/main/java/com/secao01/secao01/model/Animal.java
+++
@@ -1,5 +0,0 @@
-package com.secao01.secao01.model;
-
-public interface Animal {
-    void fazerBarulho();
-}
--- src/main/java/com/secao01/secao01/model/Cliente.java
+++
@@ -1,5 +0,0 @@
-package com.secao01.secao01.model;
-
-public class Cliente {
-    private String nome;
-}
